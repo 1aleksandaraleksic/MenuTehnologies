99c1b2a77521300cc294269e9a9cbe25b6354690
Aleksandar Aleksic <aleksandar.aleksic@eng.it>
2022-10-04 14:28:26 +0200

    Created project

---
+++ MenuTehnologies/MenuTehnologies/MenuTehnologiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuTehnologiesApp.swift
+//  MenuTehnologies
+//
+//  Created by aleksandar.aleksic on 4.10.22..
+//
+
+import SwiftUI
+
+@main
+struct MenuTehnologiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
